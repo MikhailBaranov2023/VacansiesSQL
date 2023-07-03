9572f665462b40ac39bd950e7999946c4861edb9
Mikhail Baranov <problemaprod@gmail.com>
2023-07-04 00:20:06 +0100

    Описал файл readme и добавил интерфейс для работы с sql запросами

--- src/main.py
+++ src/main.py
@@ -25,11 +25,35 @@ if __name__ == '__main__':
     insert_employer_data_to_db(employers_list, db)
     insert_vacancy_data_to_db(vacancy_list, db)
 
-    print(db.get_companies_and_vacancies_count())
-    print(db.get_all_vacancies())
-    print(db.get_avg_salary())
-    print(db.get_vacancies_with_higher_salary())
-    print(db.get_vacancies_with_keyword("Фасовщик"))
+    user_input = int(input("""
+        Доступны следующие функции:
+        1 - получаете список всех компаний и количество вакансий у каждой компании.
+        2 - получаете список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
+        3 - получаете среднюю зарплату по вакансиям.
+        4 - получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
+        5 - получает список всех вакансий, в названии которых содержатся переданное слово.
+        0 - Выход.
+    """))
+    print(user_input)
+    while True:
+        if user_input == 1:
+            print(db.get_companies_and_vacancies_count())
+            user_input = int(input())
+        elif user_input == 2:
+            print(db.get_all_vacancies())
+            user_input = int(input())
+        elif user_input == 3:
+            print(db.get_avg_salary())
+            user_input = int(input())
+        elif user_input == 4:
+            print(db.get_vacancies_with_higher_salary())
+            user_input = int(input())
+        elif user_input == 5:
+            input_word = str(input("Введите слово для поиска").upper())
+            print(db.get_vacancies_with_keyword(input_word))
+            user_input = int(input(""))
+        elif user_input == 0:
+            break
 
     if not db.conn.closed:
         db.conn.close()
